c665a9633f396a60a1c838455d03361c017ced49
Nikigy <59452769+Nikigy@users.noreply.github.com>
2023-11-06 14:57:46 -0600

    Added scroll button to home page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector('.hero-title').classList.add('fade-in');
+
+    document.querySelector('.scroll-down-button').addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector('#content').scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+window.addEventListener('scroll', function() {
+    var buttonContainer = document.querySelector('.hero-banner');
+    var websiteName = document.querySelector('.website-name');
+    var scrollPosition = 50; // adjust this value as needed
+
+    if (window.pageYOffset > scrollPosition) {
+        buttonContainer.classList.add('hero-banner--scrolled');
+        websiteName.classList.add('website-name--visible');
+    } else {
+        buttonContainer.classList.remove('hero-banner--scrolled');
+        websiteName.classList.remove('website-name--visible');
+    }
 });
 
 window.addEventListener('scroll', function() {
