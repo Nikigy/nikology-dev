42a0dd2a5c5255ca7d057c97739559d800a4be4c
Nikigy <59452769+Nikigy@users.noreply.github.com>
2023-11-11 18:14:07 -0600

    Update CSS and JS files

--- js/script.js
+++ js/script.js
@@ -1,36 +1,55 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector('.hero-title').classList.add('fade-in');
+    let heroTitle = document.querySelector('.hero-title');
+    let scrollDownButton = document.querySelector('.scroll-down-button');
+    let buttonContainer = document.querySelector('.hero-banner');
+    let websiteName = document.querySelector('.website-name');
+    let modalClose = document.querySelector('.modal-close');
+    let modal = document.querySelector('.modal');
+    let contentContainer = document.querySelector('.content-container');
 
-    document.querySelector('.scroll-down-button').addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector('#content').scrollIntoView({ behavior: 'smooth' });
-    });
-});
-
-window.addEventListener('scroll', function() {
-    var buttonContainer = document.querySelector('.hero-banner');
-    var websiteName = document.querySelector('.website-name');
-    var scrollPosition = 50; // adjust this value as needed
-
-    if (window.pageYOffset > scrollPosition) {
-        buttonContainer.classList.add('hero-banner--scrolled');
-        websiteName.classList.add('website-name--visible');
-    } else {
-        buttonContainer.classList.remove('hero-banner--scrolled');
-        websiteName.classList.remove('website-name--visible');
+    if (heroTitle) {
+        heroTitle.classList.add('fade-in');
     }
-});
 
-window.addEventListener('scroll', function() {
-    var buttonContainer = document.querySelector('.hero-banner');
-    var websiteName = document.querySelector('.website-name');
-    var scrollPosition = 50; // adjust this value as needed
+    if (scrollDownButton) {
+        scrollDownButton.addEventListener('click', function(e) {
+            e.preventDefault();
+            let contentTop = document.querySelector('#content').offsetTop;
+            let bannerHeight = document.querySelector('.hero-banner').offsetHeight;
+            window.scrollTo({ top: contentTop - bannerHeight, behavior: 'smooth' });
+        });
+    }
 
-    if (window.pageYOffset > scrollPosition) {
-        buttonContainer.classList.add('hero-banner--scrolled');
-        websiteName.classList.add('website-name--visible');
-    } else {
-        buttonContainer.classList.remove('hero-banner--scrolled');
-        websiteName.classList.remove('website-name--visible');
+    window.addEventListener('scroll', function() {
+        let scrollPosition = 50; // adjust this value as needed
+
+        if (window.pageYOffset > scrollPosition) {
+            if (buttonContainer) buttonContainer.classList.add('hero-banner--scrolled');
+            if (websiteName) websiteName.classList.add('website-name--visible');
+        } else {
+            if (buttonContainer) buttonContainer.classList.remove('hero-banner--scrolled');
+            if (websiteName) websiteName.classList.remove('website-name--visible');
+        }
+    });
+
+    document.querySelectorAll('.image-container').forEach(item => {
+        item.addEventListener('click', function() {
+            let modalImage = document.querySelector('.modal-image');
+            let modalDescription = document.querySelector('.modal-description');
+
+            if (modal && modalImage && modalDescription) {
+                modal.style.display = 'block';
+                modalImage.src = this.querySelector('img').src;
+                modalDescription.innerHTML = this.querySelector('.image-description').innerHTML;
+                if (contentContainer) contentContainer.classList.add('blurred');
+            }
+        });
+    });
+
+    if (modalClose && modal) {
+        modalClose.addEventListener('click', function() {
+            modal.style.display = 'none';
+            if (contentContainer) contentContainer.classList.remove('blurred');
+        });
     }
 });
